8c644e6d912fa01583c154b764489ad06e857086
senshreyas98 <56870652+senshreyas98@users.noreply.github.com>
2022-02-03 00:00:22 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,24 @@
+#Add implementation
+def add(x,y):
+    return x+y;
+
+#Subtraxt implementation
+def subtract(x,y):
+    return x-y;
+
+#Multiply implementation
+def multipy(x,y):
+    return x*y;
+
+#Divide implementation
+def divide(x,y):
+    if y==0:
+        return Div_by_0_Error;
+    else:
+        return x/y;
+    888
+    pagalpanti
+    chutiyapanti
+
+def square(x):
+    pass;
